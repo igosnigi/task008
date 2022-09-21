4f55386304fc95828977c448ff9bd67b28327b2d
igor <igor.kot.snig@yandex.ru>
2022-09-21 10:38:44 +0500

    последнее задание

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int i = 2;
+
+for (i = 2; i<= number;)
+{
+    Console.Write($"{i}, ");
+    i=i+2;
+}
